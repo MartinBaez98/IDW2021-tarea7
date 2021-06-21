90e8847cebbce3197e1ef82aae84f2343b8dfe70
Baez <maallopezba@ittepic.edu.mx>
2021-06-21 00:54:32 -0600

    CRUD

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var schema = require('./squema.js');
 
-mongoose.connect('mongodb://localhost:27017/eje05');
+mongoose.connect('mongodb://localhost:27017/tarea07');
 
 var Book = mongoose.model('Book',schema,'books');
 
@@ -25,3 +25,35 @@ book.save(function(error) {
     } 
     console.log("Se guardó correctamente");
  });
+
+ Book.update({_id:'60d03681e53f7a1db00e6502'}, {$set:{hidden: true}},
+function (error, docs) {
+    if(error){
+        console.log(error);
+        process.exit(1);
+    }
+    console.log("<----Actualizacion---->");
+    console.log(docs);
+    //process.exit(0);
+});
+
+Book.findOneAndRemove({_id:'60d03681e53f7a1db00e6502'}, function(error, docs) {
+    if(error){
+        console.log(error);
+        process.exit(1);
+    }
+    console.log("Libro eliminado");
+    console.log(docs);
+    process.exit(0);
+});
+
+Book.find({author: "J. R. R. Tolkien"}, 
+function (error, docs) {
+    if(error){
+        console.log(error);
+        process.exit(1);
+    }
+console.log("--Consulta--");
+console.log(docs);
+process.exit(0);
+});
